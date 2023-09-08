d097eb8d646c1aeaa5065bfe4cc29c3ee3ed45e8
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-09-08 17:51:03 +0500

    JS

---
+++ app.js
@@ -0,0 +1,141 @@
+// ChapterNo1
+
+// Q1
+
+alert("Hello")
+
+// Q2
+
+alert("Error! Please enter a valid password")
+
+// Q3
+
+alert("Welcome to JS land \nHappy Coding!")
+
+// Q4
+
+alert("Welcome to JS land!")
+
+alert("Welcome to JS land!")
+
+// Q5
+
+alert("Hello... I can run JS through my web browser's console")
+
+
+// ChapterNo2
+
+// Q1
+
+var username = "Jhone ";
+
+// Q2
+
+var myName = "Jhone Doe";
+
+// Q3
+
+var message;
+message = "Hello, World!";
+alert(message);
+
+// Q4
+
+// Declare variables for bio data
+var studentName = "John Doe";
+var studentAge = 20;
+var studentMajor = "Certified Mobile Application Development";
+
+
+// Display the bio data in alert boxes
+alert("studentName");
+alert("studentAge");
+alert("StudentMajor");
+
+
+// Q5
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+// Q6
+
+var email = "example@example.com";
+alert(message);
+
+// Q7
+
+var book = "A smarter way to learn JavaScript";
+alert(book);
+
+// Q8
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+
+// Chapter3
+
+// Q1
+
+var age = 15; 
+alert(age);
+
+// Q2
+
+var Visited = You have visited this site 15 times; 
+alert(Visited);
+
+// Q3
+
+var dateofbirth = "my birth year is 1990";
+
+
+// Chapter4
+
+
+// Q1
+
+var variable1
+var variable2
+var variable3
+
+// Q2
+
+// LegalVariable:
+
+firstName
+_counter
+$price
+user123
+myVar
+
+
+// IllegalVariable:
+
+123user
+first name
+my-variable
+var 
+my variable 
+
+// Q3
+
+// A variable name can contain only letters, numbers, dollar signs, and underscores.
+
+// Q4
+
+// valid variable names include $name, _name, or $name.
+
+// Q5
+
+
+
+
+
+
+
+
+
+
+
+
+
